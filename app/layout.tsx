8fa2493bb0b7581c8ecf549a457064dd3ed865d5
Your Name <swiftwilliams868@gmail.com>
2026-02-01 12:06:31 +0000

    feat: initialize project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner"
+import BetterAuthUIProvider from "@/providers/better-auth-ui-provider"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +26,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+     <BetterAuthUIProvider>{children}</BetterAuthUIProvider>
+        <Toaster />
       </body>
     </html>
+    
   );
 }
